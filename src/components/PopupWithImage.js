d985635b6ee3507d525e679187867313c3306a11
Polina Fedorova <ps.fedorova@yandex.ru>
2020-07-24 23:10:41 +0500

    fix: check the card for availability

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -6,12 +6,13 @@ import ButtonClose from "./ButtonClose";
 function PopupWithImage(props) {
 
   const card = props.card;
+  if(!card) return null;
 
   return (
     <div className={`popup popup__closed popup__zoom-card ${props.isOpen  ? "popup_opened" : ""}`}>
       <div className="popup__zoom">
         <img
-          //src={`${card.link}`}
+          src={`${card.link}`}
           alt={card.name}
           className="popup__image"/>
         <ButtonClose closeHandler={props.onClose}/>
